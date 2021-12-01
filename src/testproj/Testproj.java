e589e516604603878068ec5807b3ef5b6bc5fbe4
isabels005 <isabelrsanders@gmail.com>
2021-12-01 08:36:35 -0800

    Initial commit

---
+++ src/testproj/Testproj.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproj;
+
+/**
+ *
+ * @author 384001116
+ */
+public class Testproj {
+
+    public static void main(String[] args) {
+        System.out.println("your mom joke"); 
+        ///coowjgjgr4gsokfmtgdeivbkf
+    }
+    
+}
+
+public class Class2 {
+//code
+        System.out.println("your mom joke"); 
+
+}
